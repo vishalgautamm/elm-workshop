fbab70441973547b819d7976b23238200878b75b
vishalgautamm <vishal.g@live.ca>
2017-05-10 12:44:40 -0400

    Initial Commit

---
+++ part1/Main.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main : Html msg
+main =
+    div [ class "content" ]
+        [ header []
+            [ h1 [] [ text "ElmHub" ]
+            , span
+                [ class "tagline" ]
+                [ text "Like GitHub, but for Elm things" ]
+            ]
+        ]
---
+++ part2/Main.elm
@@ -0,0 +1,44 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Header =
+    { result :
+        { id : Int
+        , stars : Int
+        , name : String
+        }
+    }
+
+
+model : Header
+model =
+    { result =
+        { id = 1
+        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+        , stars = 66
+        }
+    }
+
+
+main : Html msg
+main =
+    let
+        elmHubHeader =
+            header []
+                [ h1 [] [ text "ElmHub" ]
+                , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
+                ]
+    in
+        div [ class "content" ]
+            [ elmHubHeader
+            , ul [ class "results" ]
+                [ li []
+                    [ span [ class "star-count" ]
+                        [ text (toString model.result.stars) ]
+                    , a [ href ("https://github.com/" ++ model.result.name) ] [ text "Elm" ]
+                    ]
+                ]
+            ]
